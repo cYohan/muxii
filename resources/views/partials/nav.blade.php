a7733d8b5475c6da61f09e85bc6d5fd79f24bff6
Camilo Sanchez <yohancamilosanchezm@gmail.com>
2022-02-16 17:02:12 -0500

    Random - Cambios en el nav y el app

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light navbar-socialapp">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('home') }}">
+        <a class="navbar-brand" href="/">
             MUXII
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
